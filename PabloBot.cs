954195ee2c5114b7d0d4b4727ed4cbfae80950a6
AlexBychuk <sbychuk645@gmail.com>
2021-05-19 00:45:01 +0300

    restructirization

---
+++ PabloBot.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+using DSharpPlus;
+using System.IO;
+using Newtonsoft.Json;
+using PabloBot.Services;
+using Microsoft.Extensions.Logging;
+
+namespace PabloBot
+{
+    class PabloBot
+    {
+        DiscordClient Client { get; set; }
+        CommandsNextExtension Commands { get; set; }
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("../../../config.json"))
+            {
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                {
+                    json = await sr.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug,
+            };
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient k, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,13 @@
 ﻿using System;
-using Discord;
-using Discord.WebSocket;
-using Discord.Commands;
-using System.Threading.Tasks;
 
 namespace PabloBot
 {
     class Program
     {
-        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
-
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        private CommandHandler installCommands;
-
-        public async Task MainAsync()
+        static void Main(string[] args)
         {
-            _client = new DiscordSocketClient();
-            _commands = new CommandService();
-            _client.Log += Log;
-            installCommands = new CommandHandler(_client, _commands);
-            var token = "Nzk5NzE5Njg2MzUwMTc2Mjg3.YAHq_w.OemWw2KXzwBGZ2amkQ4XTh4-SkI";
-            await installCommands.InstallCommandsAsync();
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-            await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage log)
-        {
-            Console.WriteLine(log.ToString());
-            return Task.CompletedTask;
+            var bot = new PabloBot();
+            bot.RunAsync().GetAwaiter().GetResult();
         }
     }
 }
