894f920659ede36e792670b7a5a1cfe4b7024600
AlexBychuk <sbychuk645@gmail.com>
2021-05-20 16:25:29 +0300

    First steps to DB and Lvl

---
+++ Services/Models/PabloContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PabloBot.Services.Models.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PabloBot.Services.Models
+{
+    class PabloContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pablodb;Username=postgres;Password=postgres");
+        }
+    }
+}
---
+++ Services/Models/Profiles/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PabloBot.Services.Models.Profiles
+{
+    class Profile : Entity
+    {
+        public ulong DiscordId { get; set; }
+        public ulong GuildId { get; set; }
+        public int Xp { get; set; }
+    }
+}
